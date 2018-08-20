53b2873393cdd24d21d62499b091958ec41264d3
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-08-20 14:22:04 -0600

    Review of scriptable objects and inheritance

---
+++ 2670Fall2018/Assets/IfStatements.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class IfStatements : MonoBehaviour
+{
+	public float speed;
+
+	public bool lightsOn;
+	
+	// Use this for initialization
+	void Start () {
+		if (lightsOn)
+		{
+			//do work
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
