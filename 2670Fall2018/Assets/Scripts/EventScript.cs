74a8329a6993aa99bc0905576d5671203e20231d
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-08-27 14:26:32 -0600

    Worked on events and triggers

---
+++ 2670Fall2018/Assets/Scripts/EventScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class EventScript : MonoBehaviour {
+
+	 public UnityEvent MyEvent;
+
+	private void OnMouseDown()
+	{
+		MyEvent.Invoke();
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		MyEvent.Invoke();
+	}
+}
