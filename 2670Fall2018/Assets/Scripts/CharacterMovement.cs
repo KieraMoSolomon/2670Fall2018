012e36153c7e6a5c5a90aaa2b92b8a045ab8a3ce
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-09-10 14:24:27 -0600

    Awesome Movement script

--- 2670Fall2018/Assets/Scripts/CharacterMovement.cs
+++ 2670Fall2018/Assets/Scripts/CharacterMovement.cs
@@ -5,30 +5,16 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
 
-    public MovePattern Movement;
-
-    public FloatData MoveX, MoveY, MoveZ;
-    
-    private Vector3 moveDirection = Vector3.zero;
     private CharacterController controller;
-
+    public MovePattern Pattern;
     
     private void Start()
     {
         controller = GetComponent<CharacterController>();
     }
 
-    void Update() {
-        
-        if (controller.isGrounded) {
-            moveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.Value);
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= Movement.speed;
-            if (Input.GetButton("Jump"))
-                moveDirection.y = Movement.jumpSpeed;
-            
-        }
-        moveDirection.y -= Movement.gravity * Time.deltaTime;
-        controller.Move(moveDirection * Time.deltaTime);
+    void Update()
+    {
+        Pattern.Invoke(controller, transform);
     }
 }
