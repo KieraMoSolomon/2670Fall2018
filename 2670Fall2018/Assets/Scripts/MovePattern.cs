10962b1deb52de822321368b82193fba4c1d27a1
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-09-12 14:22:23 -0600

    Got the movement to not need to be on the ground, still no double jump

--- 2670Fall2018/Assets/Scripts/MovePattern.cs
+++ 2670Fall2018/Assets/Scripts/MovePattern.cs
@@ -3,30 +3,39 @@ using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu]
 public class MovePattern : ScriptableObject {
-    public FloatData jumpSpeed;
 
     public FloatData MoveX, MoveY, MoveZ;
     public FloatData gravity;
     public FloatData RotX, RotY, RotZ;
     
-    private Vector3 moveDirection;
-    private Vector3 rotDirection;
+    protected Vector3 moveDirection;
+    protected Vector3 rotDirection;
 
-    public void Invoke(CharacterController controller, Transform transform)
+    public virtual void Invoke(CharacterController controller, Transform transform)
     {
-        if (controller.isGrounded) {
-            moveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.Value);
-            
-            rotDirection.Set(RotX.Value, RotY.Value, RotZ.Value);
-            transform.Rotate(rotDirection);
-            
-            moveDirection = transform.TransformDirection(moveDirection);
-            
-            if (Input.GetButton("Jump"))
-                moveDirection.y = jumpSpeed.Value;
-            
+        if (controller.isGrounded)
+        {
+
+            Move(transform);
         }
+
+        Move(controller);
+    }
+
+    protected void Move(CharacterController controller)
+    {
         moveDirection.y -= gravity.Value * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    protected void Move(Transform transform)
+    {
+        moveDirection.Set(MoveX.Value, MoveY.Value, MoveZ.Value);
+            
+        rotDirection.Set(RotX.Value, RotY.Value, RotZ.Value);
+        transform.Rotate(rotDirection);
+            
+        moveDirection = transform.TransformDirection(moveDirection);
+
     }
 }
---
+++ 2670Fall2018/Assets/Scripts/MovePatternDual.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[CreateAssetMenu]
+public class MovePatternDual : MovePattern
+{
+
+	private bool doubleJump;
+	
+	public override void Invoke(CharacterController controller, Transform transform)
+	{
+		if (controller.isGrounded)
+		{
+			Move(transform);
+			doubleJump = true;
+		}
+		else
+		{
+			if (doubleJump)
+			{
+				Move(transform);
+				doubleJump = false;
+			}
+		}
+
+		Move(controller);
+	}
+
+	void SetJump()
+	{
+		doubleJump = true;
+	}
+}
