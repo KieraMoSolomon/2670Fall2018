cddaf236461c206dc6ce80fd5c50d8696add7b58
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-09-21 00:00:16 -0600

    Trying to do a good weapons options for a game

---
+++ 2670Fall2018/Assets/Scripts/AIAgent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class AIAgent : MonoBehaviour
+{
+
+	private NavMeshAgent agent;
+	private Transform finalDestination;
+
+	public Transform Destination;
+	public Transform PostPoint;
+
+	
+	// Use this for initialization
+	void Start ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	private void Update()
+	{
+		agent.destination = finalDestination.position;
+	}
+
+
+	private void OnTriggerExit(Collider other)
+	{
+		finalDestination = PostPoint;
+	}
+
+	private void OnTriggerEnter(Collider obj)
+	{
+		if (obj.transform == Destination)
+		{
+			finalDestination = Destination;
+		}
+	}
+}
