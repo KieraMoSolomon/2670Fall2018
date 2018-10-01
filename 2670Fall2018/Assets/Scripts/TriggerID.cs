b1dcbd6b5ea09897e7a4ac4c5fc1d2e5fab20022
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-10-01 14:36:34 -0600

    working on checking matches

---
+++ 2670Fall2018/Assets/Scripts/TriggerID.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class TriggerID : MonoBehaviour
+{
+
+	public NameID ID;
+	public UnityEvent onMactch;
+	
+	private void OnTriggerEnter(Collider obj)
+	{
+		if (ID == obj.GetComponent<ObjectID>().ID)
+		{
+			onMactch.Invoke();
+			print("Match");
+		}
+	}
+}
