7029015e7d7996fa618c98f892334bb6eed7e6af
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-10-03 14:27:28 -0600

    working on color changing on match

---
+++ 2670Fall2018/Assets/Scripts/MatchID.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class MatchID : MonoBehaviour
+{
+
+	public NameID ID;
+	public UnityEvent onMactch;
+	
+	public void Invoke(NameID id)
+	{
+		if (ID == id)
+		{
+			onMactch.Invoke();
+		}
+	}
+}
