d13cb13deda1c81bc0f9b40c20d3dd24e43145c6
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-10-30 23:02:37 -0600

    Got a lot of game made

---
+++ 2670Fall2018/Assets/Scripts/IntData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class IntData : ScriptableObject
+{
+    public int value;
+
+    public int Value
+    {
+        get { return value; }
+        set { this.value = value; }
+    }
+}
