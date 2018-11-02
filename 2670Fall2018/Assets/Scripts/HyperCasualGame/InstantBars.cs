3e3d0f389cd303914798c942b8ac4307b2bb77d4
Kiera Solomon <kieramorgensolomon@gmail.com>
2018-11-01 22:44:38 -0600

    working on instants of the bars

---
+++ 2670Fall2018/Assets/Scripts/HyperCasualGame/InstantBars.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantBars : MonoBehaviour
+{
+
+	public float Seconds = 2.0f;
+	
+	IEnumerator CreateBars()
+	{
+		yield return new WaitForSeconds(Seconds);
+	}
+}
